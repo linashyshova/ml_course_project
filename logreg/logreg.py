1c438691fa6fa0692fbc1dd3c5d68be27953881f
Lina Shyshova <lina.shyshova@booking.com>
2023-03-24 14:02:07 +0100

    Add knn implementation

---
+++ logreg/logreg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 01:37:05 2023
+
+@author: mpetrov
+"""
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from sklearn.linear_model import LogisticRegression
+
+data = pd.read_csv('../data/train.csv')
+
+train_set = data.sample(frac = 0.9)
+train_set_features = train_set.loc[:, train_set.columns != 'label']
+
+validation_set = data.drop(train_set.index)
+validation_set_features = validation_set.loc[:, validation_set.columns != 'label']
+
+# logreg = LogisticRegression(max_iter=1000000000, multi_class='multinomial', solver='newton-cg').fit(train_set_features, train_set['label'])
+
+# with open('model_serialized', 'wb') as ouf:
+#     pickle.dump(logreg, ouf)
+
+logreg = 0
+with open('model_serialized', 'rb') as inpf:
+    logreg = pickle.load(inpf)
+
+logreg.predict(validation_set_features)
